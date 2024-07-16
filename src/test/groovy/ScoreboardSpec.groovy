b54719dab46192bcb33722665c662cd3c104d213
Em Majka <maciejmajka001@gmail.com>
2024-07-16 07:33:57 +0200

    introduction of game ID - tests passing

--- src/test/groovy/ScoreboardSpec.groovy
+++ src/test/groovy/ScoreboardSpec.groovy
@@ -13,13 +13,16 @@ class ScoreboardSpec extends Specification {
 
         then:
         sb.getGames().size() == 1
-        sb.getGames().get("Team 1 vs Team 2") == game
+        sb.getGames().get(game.getGameId()) == game
     }
 
-    def "when adding same game more then once then it should throw an exception - only one game in-progress for teams"(){
+    def "when adding same game more then once then it should throw an exception - only one game in-progress for teams"() {
         given:
         var sb = new Scoreboard()
-        var game = Game.builder().teamOne("Team 1").teamTwo("Team 2").build()
+        var team1 = "Team 1"
+        var team2 = "Team 2"
+        var gameId = GameId.builder().teamOne(team1).teamTwo(team2).build()
+        var game = Game.builder().gameId(gameId).teamOne(team1).teamTwo(team2).build()
 
         when:
         sb.addGame(game)
