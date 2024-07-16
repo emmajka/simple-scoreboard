7c46953659629cc5052df9fc5421d41e37ba35d5
Em Majka <maciejmajka001@gmail.com>
2024-07-16 07:14:20 +0200

    addGame test duplicates - failing

--- src/test/groovy/ScoreboardSpec.groovy
+++ src/test/groovy/ScoreboardSpec.groovy
@@ -2,7 +2,7 @@ import spock.lang.Specification
 
 class ScoreboardSpec extends Specification {
 
-    def "when adding a new game it should update games collection correctly"() {
+    def "when adding a new game then it should update games collection correctly"() {
         given:
         var sb = new Scoreboard()
         var game = Game.builder().teamOne("Team 1").teamTwo("Team 2").build()
@@ -13,5 +13,18 @@ class ScoreboardSpec extends Specification {
         then:
         sb.getGames().size() == 1
         sb.getGames().get("Team 1 vs Team 2") == game
+    }
+
+    def "when adding same game more then once then it should throw an exception - only one game in-progress for teams"(){
+        given:
+        var sb = new Scoreboard()
+        var game = Game.builder().teamOne("Team 1").teamTwo("Team 2").build()
+
+        when:
+        sb.addGame(game)
+        sb.addGame(game)
+
+        then:
+        thrown(Exception)
     }
 }
