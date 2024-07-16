ea08f8584bd953310b39cf5618f41f0d4b98b7b0
Em Majka <maciejmajka001@gmail.com>
2024-07-16 08:47:36 +0200

    game update score, negative values - tests passing

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,5 +1,6 @@
 package game;
 
+import exception.IncorrectGameScoreException;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,7 +16,11 @@ public class Game {
     private int teamOneScore;
     private int teamTwoScore;
 
-    public void updateScore(int newTeamOneScore, int newTeamTwoScore) {
+    public void updateScore(int newTeamOneScore, int newTeamTwoScore) throws IncorrectGameScoreException {
+        if (newTeamOneScore < 0 || newTeamTwoScore < 0) {
+            var errMsg = String.format("%s vs %s game score could not be updated! Setting negative values is prohibited!", newTeamOneScore, newTeamTwoScore);
+            throw new IncorrectGameScoreException(errMsg);
+        }
         this.teamOneScore = newTeamOneScore;
         this.teamTwoScore = newTeamTwoScore;
     }
